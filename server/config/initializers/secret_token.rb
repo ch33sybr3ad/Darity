66a89cef29b84e913e69dc1a8ae87c00c18e8178
Jason Zhao & Nam Hoang Nguyen <pair+ch33sybr3ad.nguyennam9696@devbootcamp.com>
2015-05-28 13:08:00 -0700

    created clinet and server

---
+++ server/config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+Server::Application.config.secret_token = 'e046e0e00bf92e6ed5ebcf18fdf2123ccdea6a98c964dc7380ead9a5f3cb95283fe7a04943f8f333406668b56141bdaaf004ecfe59ec862fd77cef1aca45882a'
