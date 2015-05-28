fb7d12e99004c03f27cb249950ba05da76fb75bb
Jason Zhao & Nam Hoang Nguyen <pair+ch33sybr3ad.nguyennam9696@devbootcamp.com>
2015-05-28 14:06:38 -0700

    working on relationships

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
 
   has_many :relationships
-  has_many :dares, through: :relationships, foreign_key: :pledger_id
+  has_many :pledged_dares, through: :relationships, class_name: "Dare"
 
-  has_many :dares, foreign_key: :daree_id, class_name: "User"
-  has_many :dares, foreign_key: :proposer_id, class_name: "User"
+  # has_many :challenged_dares, foreign_key: :daree_id, class_name: "Dare"
+  # has_many :proposed_dares, foreign_key: :proposer_id, class_name: "Dare"
 
 end
