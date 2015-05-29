e0a59e0680f4ab677b7831919fedfec7b02e72d9
Andrew Carlson <acarl005@g.ucla.edu>
2015-05-28 17:34:37 -0700

    index and show pages

--- client/app/controllers/dares_controller.rb
+++ client/app/controllers/dares_controller.rb
@@ -1,4 +1,12 @@
 class DaresController < ApplicationController
+
   def index
+    @user = User.find(params[:user_id])
+    @dares = @user.challenged_dares
+  end
+
+  def show
+    @dare = Dare.find(params[:id])
+    @proposer = @dare.proposer
   end
 end
