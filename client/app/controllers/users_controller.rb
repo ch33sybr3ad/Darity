119ec57d08aba31c7ba8fd145c1c81a97957b3d3
Jason Zhao & Nam Hoang Nguyen <pair+ch33sybr3ad.nguyennam9696@devbootcamp.com>
2015-05-28 20:39:03 -0700

    finish form

--- client/app/controllers/users_controller.rb
+++ client/app/controllers/users_controller.rb
@@ -2,9 +2,9 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(session[:user_id])
-    @challenged_dares = @user.challenged_dares
-    @proposed_dares = @user.proposed_dares
-    @pledged_dares = @user.pledged_dares
+    p @user
+    p '####################'
+    retreive_all_dares
   end
 
   def home
@@ -16,11 +16,22 @@ class UsersController < ApplicationController
   end
 
   def login
-    @user = User.where(email: params[:email]).first
-    if @user && @user.password == params[:password]
+    @user = User.where(email: user_params["email"]).first
+    if @user && @user.password == user_params["password"]
       session[:user_id] = @user.id
-      render "show"
+      redirect_to @user
     end
   end
 
+  private
+
+  def retreive_all_dares
+    p @challenged_dares = @user.challenged_dares
+    p @proposed_dares = @user.proposed_dares
+    p @pledged_dares = @user.pledged_dares
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 end
