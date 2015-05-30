f09bfdda705893b4e1aa70b4878653b220dba6e9
Andrew Carlson <acarl005@g.ucla.edu>
2015-05-30 16:39:16 -0700

    helper method improve

--- client/app/helpers/application_helper.rb
+++ client/app/helpers/application_helper.rb
@@ -1,6 +1,11 @@
 module ApplicationHelper
   def tweet_button(args)
-    link_to args[:message], "https://twitter.com/intent/tweet?text=%23Viral%20#{args[:dare].title}%20-%20dared%20to%20%40#{args[:dare].daree.username}%20proposed%20by%20%40#{args[:dare].proposer.username}%20%23Darity%20%23Dare%20For%20%23Charity%20see%20dare%20here%20localhost:3000/users/#{args[:user].id}/dares/#{args[:dare].id}&url=localhost:3000/users/#{args[:user].id}/dares/#{args[:dare].id}", "data-via" => "#{args[:dare].proposer}", "data-related" => "#{args[:dare].daree}", class: 'btn btn-primary'
+    begin
+      target = args[:dare].daree.username
+    rescue
+      target = args[:dare].twitter_handle
+    end
+    link_to args[:message], "https://twitter.com/intent/tweet?text=%20#{args[:dare].title}%20-%20dared%20to%20%40#{target}%20by%20%40#{args[:dare].proposer.username}%20%23Darity%20%23Dare%20For%20%23Charity%20see%20dare%20here%20localhost:3000/users/#{args[:user].id}/dares/#{args[:dare].id}&url=localhost:3000/users/#{args[:user].id}/dares/#{args[:dare].id}", class: 'btn btn-primary'
   end
 end
 
