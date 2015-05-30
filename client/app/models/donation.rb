d2e6365b7937cc21bf2ab0e4016825574ec5a5f7
Andrew Carlson <acarl005@g.ucla.edu>
2015-05-29 23:26:44 -0700

    controller tests

--- client/app/models/donation.rb
+++ client/app/models/donation.rb
@@ -1,4 +1,7 @@
 class Donation < ActiveRecord::Base
   belongs_to :user, foreign_key: "pledger_id"
   belongs_to :dare, foreign_key: "pledged_dare_id"
+
+  validates_presence_of :pledger_id, :pledged_dare_id, :donation_amount
+  validates :donation_amount, numericality: true
 end
