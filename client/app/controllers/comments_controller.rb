6d275f25f40d2c67b20a7cd7c378d0297dcdd179
Jason <jzhao.690@gmail.com>
2015-06-01 11:55:27 -0700

    updated the form page

--- client/app/controllers/comments_controller.rb
+++ client/app/controllers/comments_controller.rb
@@ -1,2 +1,15 @@
 class CommentsController < ApplicationController
+
+  def create
+    @comment = Comment.create(comment_params)
+    #needs a redirect to somewhere!
+    
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body, :user_id, :dare_id)
+  end
+
 end
