5757778db78e10ad29e45eacbf71759717524a02
Andrew Carlson <acarl005@g.ucla.edu>
2015-06-02 13:16:03 -0700

    jumbo hero

--- client/app/assets/javascripts/signup.js
+++ client/app/assets/javascripts/signup.js
@@ -1,44 +1,39 @@
-var watchForm = function() {
-  $('.signup-link a').on('click', function(e){
-    e.preventDefault();
+$(document).ready(function() {
 
-    $('.login-area').hide();
-    $('.signup-area').show();
+  $('#show-signup').on('click', function(e) {
+    e.preventDefault();
+    $('a.dropdown-toggle.signup').dropdown('toggle');
   });
 
-  $('.login-link a').on('click', function(e){
+  $('#show-login').on('click', function(e) {
     e.preventDefault();
-
-    $('.signup-area').hide();
-    $('.login-area').show();
+    $('a.dropdown-toggle.login').dropdown('toggle');
   });
 
-  $('.new_user').on('submit', function(e){
-    e.preventDefault();
-    if ($(this).find('input.signup-password').val() === $(this).find('input.signup-password-confirm').val()) {
-      $('.new_user').off('submit').submit();
-    } else {
-      $('.alert').show();
-      $('.alert').text('Password Not Matching');
-    }
+  var watchForm = function() {
+    $('.new_user').on('submit', function(e){
+      e.preventDefault();
+      if ($(this).find('input.signup-password').val() === $(this).find('input.signup-password-confirm').val()) {
+        $('.new_user').off('submit').submit();
+      } else {
+        $('.alert').show();
+        $('.alert').text('Password Not Matching');
+      }
+    });
+  };
 
-  });
-};
+  var editForm = function() {
+    $('.edit_user').on('submit', function(e){
+      e.preventDefault();
+      var new_pw = $(this).find('[name="user[new_password]"]').val()
+      var new_pw2 = $(this).find('[name="user[confirm_password]"]').val()
 
-var editForm = function() {
-  $('.edit_user').on('submit', function(e){
-    e.preventDefault();
-    var new_pw = $(this).find('[name="user[new_password]"]').val()
-    var new_pw2 = $(this).find('[name="user[confirm_password]"]').val()
-
-    if (!(new_pw === new_pw2)) {
-      $(this).find('[name="user[new_password]"]').css("border-color", "red")
-      $(this).find('[name="user[confirm_password]"]').css("border-color", "red")
-    } else {
-        $('.edit_user').off('submit').submit();
-    };
-  });
-}
-
-$(document).on('ready', watchForm);
-$(document).on('ready', editForm);
+      if (!(new_pw === new_pw2)) {
+        $(this).find('[name="user[new_password]"]').css("border-color", "red")
+        $(this).find('[name="user[confirm_password]"]').css("border-color", "red")
+      } else {
+          $('.edit_user').off('submit').submit();
+      };
+    });
+  };
+});
