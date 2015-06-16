d0e9287b8ec4a8e76c1aa56965c42946b8dafc35
Andrew Carlson <acarl005@g.ucla.edu>
2015-06-15 17:20:29 -0700

    refactor form watchers

--- app/assets/javascripts/signup.js
+++ app/assets/javascripts/signup.js
@@ -1,36 +1,33 @@
-  var watchForm = function() {
-    $('.new_user').on('submit', function(e){
-      e.preventDefault();
-      if ($(this).find('input.signup-password').val() === $(this).find('input.signup-password-confirm').val()) {
-        $('.new_user').off('submit').submit();
-      } else {
-        $('.alert').show();
-        $('.alert').text('Password Not Matching');
-      }
-    });
-  };
+var watchForm = function() {
+  $('.new_user').on('submit', function(e){
+    e.preventDefault();
+    if ($(this).find('input.signup-password').val() === $(this).find('input.signup-password-confirm').val()) {
+      $('.new_user').off('submit').submit();
+    } else {
+      $('.alert').show();
+      $('.alert').text('Password Not Matching');
+    }
+  });
+};
 
-  var editForm = function() {
-    $('.edit_user').on('submit', function(e){
-      e.preventDefault();
-      var new_pw = $(this).find('[name="user[new_password]"]').val()
-      var new_pw2 = $(this).find('[name="user[confirm_password]"]').val()
-      debugger;
-      if (!(new_pw === new_pw2)) {
-        $(this).find('[name="user[new_password]"]').css("border-color", "red")
-        $(this).find('[name="user[confirm_password]"]').css("border-color", "red")
-      } else {
-          $('.edit_user').off('submit').submit();
-      };
-    });
-  };
+// var editForm = function() {
+//   $('.edit_user').on('submit', function(e){
+//     e.preventDefault();
+//     var new_pw = $(this).find('[name="user[new_password]"]').val();
+//     var new_pw2 = $(this).find('[name="user[confirm_password]"]').val();
+//     if (!(new_pw === new_pw2)) {
+//       $(this).find('[name="user[new_password]"]').css("border-color", "red")
+//       $(this).find('[name="user[confirm_password]"]').css("border-color", "red")
+//     } else {
+//       $('.edit_user').off('submit').submit();
+//     }
+//   });
+// };
 
 $(document).ready(function() {
 
   watchForm();
-  editForm();
-
-  $('body').scrollspy({ target: '#navbar' })
+  // editForm();
 
   $('#show-signup').on('click', function(e) {
     e.preventDefault();
